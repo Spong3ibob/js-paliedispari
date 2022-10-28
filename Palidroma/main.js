2f4b9ec245b465c06b687a39a5b846f0c54abfed
Spong3ibob <leonardomaran17@gmail.com>
2022-10-28 15:40:05 +0200

    esercizzio palidroma

---
+++ Palidroma/main.js
@@ -0,0 +1,15 @@
+const str = prompt("Inserisci una parola");
+
+function palindrome(str) {
+
+    let len = str.length;
+    let mid = Math.floor(len/2);
+
+    for ( let i = 0; i < mid; i++ ) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
